a38344088bd47680b59931610b7d9f38f06bcfad
yos <yos.c.bd@gmail.com>
2022-08-16 12:49:13 +0300

    weather api blcok using json and useBlockProps

---
+++ YBD-gutenberg block for displaying weather data from weather api with block-json and useBlockProps/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '0f3f8788ca1526001894');
---
+++ YBD-gutenberg block for displaying weather data from weather api with block-json and useBlockProps/readme.txt
@@ -0,0 +1,1 @@
+this  version of the plugoin is registering the block via blocks.json and uses useBlockProps.
